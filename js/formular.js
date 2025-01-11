27870f3111e66f64229c787d35560218e9819865
GeraltOfRiviaa <samuelsvob@gmail.com>
2025-01-11 10:10:05 +0100

    Finals!!!

--- js/formular.js
+++ js/formular.js
@@ -24,7 +24,7 @@ let cyklus = 0;
     if(prazdnaHra == 1){
         alert('Nemůžeš mít prázdné pole hra');
         prazdnaHra = 0;
-        console.log(hry);
+        //console.log(hry);
     }
                 
     aktualizovatSeznam();
@@ -39,103 +39,22 @@ let cyklus = 0;
         } else if (document.getElementById('radio3').checked) {
             hry.sort((a, b) => a.hra.localeCompare(b.hra));
         }
+        aktualizovatSeznam();
         
         
-        if(cyklus == 0){
         if(hry.length > 1){
-            if (document.getElementById('radio1').checked) {
-                if(hry[0].hra == 'War Thunder'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'League of Legends'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'World of Tanks'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Minecraft'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Counter-Strike 2'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'For Honor'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Overwatch'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Fortnite'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Tom Clancy's Rainbow Six Siege"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "GTA V"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Apex Legends"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Among Us"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Call of Duty: Warzone"){
-                    hodnoceni();
-                }
-    
-            } else if (document.getElementById('radio2').checked) {
-                if(hry[0].hra == 'War Thunder'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'League of Legends'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'World of Tanks'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Minecraft'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Counter-Strike 2'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'For Honor'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Overwatch'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == 'Fortnite'){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Tom Clancy's Rainbow Six Siege"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "GTA V"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Apex Legends"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Among Us"){
-                    hodnoceni();
-                }
-                else if(hry[0].hra == "Call of Duty: Warzone"){
-                    hodnoceni();
-                }
-                
-            }
-
+            hodnoceni();
         }
         else{
             alert('Musíš mít v seznamu aspoň 2 hry');
         }
         for (let i = 0; i < hry.length; i++) {
-            for (let j = 0; j < hry.length; j++) {
+            for (let j = 1; j < hry.length; j++) {
                 if(hry[i].hodiny == hry[j].hodiny){
                     bonus = 1;
-                
+                }
+                else{
+                    bonus = 0;
                 }
                 
             }
@@ -152,15 +71,6 @@ let cyklus = 0;
             bonusE.appendChild(bonusT);
         }
             
-            cyklus = 1;
-            aktualizovatSeznam();
-    }
-    else {
-        location.reload()
-        //Kód je komplikovaný, jak kšandy, takže ano, to že se stránka resetuje po druhém stisknutí tlačítka SEŘADIT je fíčura
-    }
-
-
     });
     
     function aktualizovatSeznam() {
@@ -194,90 +104,123 @@ let cyklus = 0;
         let hodnoceniElement = document.getElementById('hodnoceni');
         let hodnoceniText = document.createElement('p');
         hodnoceniText.className = 'text-center display-3 shadow border border-5 rounded-pill border-primary'
+        let existingHodnoceniText = document.querySelector('#hodnoceni p');
+        if (existingHodnoceniText) {
+            existingHodnoceniText.remove();
+        }
+
         if (document.getElementById('radio1').checked) {
             if(hry[0].hra == 'War Thunder'){
                 hodnoceniText.innerHTML ='Tak to si musíme někdy zahrát. Jakou hraješ zemi? A ta pravá otázka...letadla nebo tanky? ';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'League of Legends'){
                 hodnoceniText.innerHTML = 'Běž ven a zkus se dotknout té zelené věci co roste na zemi. Říká se tomu tráva.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'World of Tanks'){
                 hodnoceniText.innerHTML = 'Doufám, žes to hrál tak moc dříve. Pokud to hraješ i teď, tak bys mohl třebas i dospět a zkusit tu pravou hru s tanky, nech se poddat šnekovi.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Minecraft'){
                 hodnoceniText.innerHTML = 'To musíš být ten pohodový týpek.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Counter-Strike 2'){
                 hodnoceniText.innerHTML = 'Fuuuha tak tomu se říka oddanost.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'For Honor'){
                 hodnoceniText.innerHTML = 'Respekt.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Overwatch'){
                 hodnoceniText.innerHTML = 'Wow tak to jsem nečekal. Asi už ale vím, co sleduješ na oranžovém youtubu';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Fortnite'){
                 hodnoceniText.innerHTML = 'Buď to potíš, a nebo jsi jenom pohodový týpek, co rád hraje svoji hru, která měla před pár lety peak. Tak či onak máš můj respekt.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Tom Clancy's Rainbow Six Siege"){
                 hodnoceniText.innerHTML = 'Noice taktické střílečky mám taky rád, ale nevydrželo mi to zas tak dlouho. Jaký máš asi pak rank s tímhle počtem hodin?';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "GTA V"){
                 hodnoceniText.innerHTML = 'Wow, nevím co tam děláš, ale asi nejsi moc náročný.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Apex Legends"){
                 hodnoceniText.innerHTML = 'Dobrá, tady není co namítat.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Among Us"){
                 hodnoceniText.innerHTML = 'Jdi skočit z mostu v Minecraftu';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Call of Duty: Warzone"){
                 hodnoceniText.innerHTML = 'Insane';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
-
-        } else if (document.getElementById('radio2').checked) {
+            
+        } 
+        else if (document.getElementById('radio2').checked) {
             if(hry[0].hra == 'War Thunder'){
                 hodnoceniText.innerHTML = 'Buď si insane a máš vyzkoumaných několi stromů několika národů a nebo prostě nejedeš a achievenemty :d';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'League of Legends'){
                 hodnoceniText.innerHTML = 'https://www.youtube.com/watch?v=fZxHDwKlI6I ';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'World of Tanks'){
                 hodnoceniText.innerHTML = 'Jdi si založit gaijin účet. Začni svůj grind. Bude tě to bavit';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Minecraft'){
                 hodnoceniText.innerHTML = 'Hej tak to si musel dělat hodně dlouho, ale teď zkuš všechny v KDC.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Counter-Strike 2'){
                 hodnoceniText.innerHTML = 'Však tam je jen jeden úspěch. Tak to jako je ale slabý :d';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'For Honor'){
                 hodnoceniText.innerHTML = 'Respekt++.';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Overwatch'){
                 hodnoceniText.innerHTML = 'Wow. Nemám slov :D';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == 'Fortnite'){
                 hodnoceniText.innerHTML = 'No tak takhle tu hru snad nikdo nehraje, ale taky cool';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Tom Clancy's Rainbow Six Siege"){
                 hodnoceniText.innerHTML = 'Noice to by se mi fakt nechtělo dělat';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "GTA V"){
                 hodnoceniText.innerHTML = 'Hej to musím taky zkusti, možná ta hra bude tak nachvilku zábavná';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Apex Legends"){
                 hodnoceniText.innerHTML = 'Tady se není s čím vytahovat ';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Among Us"){
                 hodnoceniText.innerHTML = 'Jdi skočit z mostu v Robloxu';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             else if(hry[0].hra == "Call of Duty: Warzone"){
                 hodnoceniText.innerHTML = 'Insane++';
+                hodnoceniElement.appendChild(hodnoceniText);
             }
             
         }
-        hodnoceniElement.appendChild(hodnoceniText);
+        
+        
     }
     function hodinyDny(){
         let hodinyCelkove = 0;
